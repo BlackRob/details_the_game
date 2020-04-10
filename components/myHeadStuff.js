7162fa229da03634665860d0a8dc0e7bccebce13
Robert Austin <robert.w.austin@gmail.com>
2020-04-10 11:34:25 +0300

    fixed meta tag

--- components/myHeadStuff.js
+++ components/myHeadStuff.js
@@ -6,7 +6,8 @@ const MyHeadStuff = ({ title }) => {
     <>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
-      <meta property="og:image" content="../../public/default_thumbnail.png" />
+      <meta property="og:image" content="/default_thumbnail.png" />
+      <meta property="fb:app_id" content="220488252548780" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://grumbly.games" />
       <meta property="og:title" content="click the image to play the game" />
