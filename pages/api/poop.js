1d68422782dcc24fc3c68ba18d0d4b0487e30e5c
Robert Austin <robert.w.austin@gmail.com>
2020-05-01 15:09:11 +0300

    cinnaminimumstantially

--- pages/api/poop.js
+++ pages/api/poop.js
@@ -1,13 +1,12 @@
-import { React } from 'react'
 import fs from 'fs'
-import { join } from 'path'
+import path from 'path'
 
 
 export default (req, res) => {
 
   let thisPoop = fs.readdirSync(join(process.cwd(), '.next/serverless/pages/api'))
   //thisPoop.push(`cwd = ${fs.readdirSync(process.cwd()).join(',')}`)
-  console.log(thisPoop.join('\n'), '\n', path.resolve('Roboto-Regular.ttf'))
+  console.log(thisPoop, path.join('\n'), '\n', path.resolve('Roboto-Regular.ttf'), '\n', (__dirname))
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
