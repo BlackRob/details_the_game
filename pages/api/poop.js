f206197bbb302cbe8902e0b4dc5a1a94ecfc1c04
Robert Austin <robert.w.austin@gmail.com>
2020-05-01 15:29:03 +0300

    shusi

--- pages/api/poop.js
+++ pages/api/poop.js
@@ -4,13 +4,13 @@ import path from 'path'
 
 export default (req, res) => {
 
-  let thisPoop = fs.readdirSync(join(process.cwd(), '.next/serverless/pages/api'))
+  let thisPoop = fs.readdirSync(path.join(process.cwd(), '.'))
   //thisPoop.push(`cwd = ${fs.readdirSync(process.cwd()).join(',')}`)
-  console.log(thisPoop, path.join('\n'), '\n', path.resolve('Roboto-Regular.ttf'), '\n', (__dirname))
+  console.log(thisPoop.join('\n'), '\n', path.resolve('Roboto-Regular.ttf'), '\n', (__dirname))
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end(thisPoop.join(','))
+  res.end(thisPoop.join('\n'), '\n', path.resolve('Roboto-Regular.ttf'), '\n', (__dirname))
 }
 
 
