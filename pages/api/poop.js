b6bfe6137211613c54d6462987b41eff16d44b22
Robert Austin <robert.w.austin@gmail.com>
2020-05-02 16:08:44 +0300

    bunglerers

--- pages/api/poop.js
+++ pages/api/poop.js
@@ -6,7 +6,7 @@ export default (req, res) => {
 
   let thisPoop = fs.readdirSync(path.join(process.cwd(), '.'))
   //thisPoop.push(`cwd = ${fs.readdirSync(process.cwd()).join(',')}`)
-  let message = "" + thisPoop.join('\n') + '\n__________' + path.resolve('./Roboto-Regular.ttf') + '______\n' + fs.readdirSync(path.join(process.cwd(), '_next')).join('\n')
+  let message = "" + thisPoop.join('\n') + '\n__________' + path.resolve('./Roboto-Regular.ttf') + '______\n' + fs.readdirSync(path.join(process.cwd(), '.next')).join('\n')
   console.log(message)
 
   res.statusCode = 200
