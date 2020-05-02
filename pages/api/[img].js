39c88b35df470f85b416ded6aa8ade08d64da7ae
Robert Austin <robert.w.austin@gmail.com>
2020-05-03 00:25:50 +0300

    sand witches are delicious

--- pages/api/[img].js
+++ pages/api/[img].js
@@ -6,9 +6,13 @@ import path from 'path'
 
 export default ({ query: { img } }, res) => {
 
-  let poop1 = fs.readdirSync(path.join(process.cwd(), 'node_modules/next/dist/next-server/lib/router/utils/'))
-  let poop2 = fs.readdirSync(path.join(process.cwd(), 'node_modules/next/dist/next-server/server/lib/'))
-  let poop3 = fs.readdirSync(path.join(process.cwd(), 'node_modules/next/dist/compiled/'))
+  let poop1 = fs.readdirSync(__dirname)
+  let poop2 = fs.readdirSync(path.join(__dirname, 'usr'))
+  let poop3 = fs.readdirSync(path.join(__dirname, 'usr/share/'))
+  //let poop3 = fs.readdirSync(path.join(process.cwd(), 'node_modules/next/dist/compiled/'))
+  //let poop1 = fs.readdirSync(path.join(process.cwd(), 'node_modules/next/dist/next-server/lib/router/utils/'))
+  //let poop2 = fs.readdirSync(path.join(process.cwd(), 'node_modules/next/dist/next-server/server/lib/'))
+  //let poop3 = fs.readdirSync(path.join(process.cwd(), 'node_modules/next/dist/compiled/'))
   let poop4 = fs.readdirSync(path.join(process.cwd(), '.next/serverless/pages/api/'))
 
   let message = "" + poop1.join('\n') + '\n_____\n' + poop2.join('\n') + '\n_____\n' + poop3.join('\n')
