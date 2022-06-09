6eee9c2747fdde33139cd78bdf7c0e2729aaee1c
Robert Austin <robert.w.austin@gmail.com>
2022-06-10 00:14:58 +0300

    issue building canvas because zlib

--- next.config.js
+++ next.config.js
@@ -3,16 +3,27 @@ module.exports = {
     cfg.module.rules.push(
       {
         test: /\.md$/,
-        loader: 'frontmatter-markdown-loader',
-        options: { mode: ['react-component'] }
+        loader: "frontmatter-markdown-loader",
+        options: { mode: ["react-component"] },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
+        use: ["file-loader"],
       }
-    )
+    );
     return cfg;
-  }
+  },
+};
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
 }
 
 /*
